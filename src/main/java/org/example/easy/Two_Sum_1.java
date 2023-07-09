05308473dc63aad7e6a590aabd68472fa33f4666
ibrokhim_ali786 <128887381+ibrokhimali786@users.noreply.github.com>
2023-07-09 10:44:45 +0500

    Completed

--- src/main/java/org/example/easy/Two_Sum_1.java
+++ src/main/java/org/example/easy/Two_Sum_1.java
@@ -6,7 +6,6 @@ public class Two_Sum_1 {
     public int[] twoSum(int[] nums, int target) {
 
         HashMap<Integer, Integer> map = new HashMap<>();
-
         for (int i = 0; i < nums.length; i++) {
             int temp = target - nums[i];
             if (map.containsKey(temp))
